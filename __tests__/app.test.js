99f377151c02f04b3e2c285f60f39bbf922c48d8
Daniel17971 <danielisa1922@gmail.com>
2023-03-30 11:54:01 +0100

    ticket 10

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -327,16 +327,16 @@ describe("PATCH /api/review/:review_id, updates votes", () => {
   });
 });
 
-describe("GET /api/users", () => {
-  it("200: responds with an array of users with properties username,name and url", () => {
-    return request(app)
-      .get("/api/users")
-      .expect(200)
-      .then((response) => {
-        const users = response.body.users;
-      });
-  });
-});
+// describe("GET /api/users", () => {
+//   it("200: responds with an array of users with properties username,name and url", () => {
+//     return request(app)
+//       .get("/api/users")
+//       .expect(200)
+//       .then((response) => {
+//         const users = response.body.users;
+//       });
+//   });
+// });
 
 afterAll(() => {
   connection.end();
